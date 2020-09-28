fce674c51f4aa063824022b8bd283095646bb213
SelVic <limelefthand@gmail.com>
2020-09-28 19:11:59 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.use('/build', express.static(path.join(__dirname, 'build')));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/fonts', express.static(path.join(__dirname, 'fonts')));
+
+app.get(['/'], function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/error', function (req, res, next) {
+    next('404040')
+})
+
+
+app.get('*', function (req, res, next) {
+    console.log(`404 ${req.url}`)
+    return res.sendStatus(404);
+})
+
+app.use(function(err, req, res, next){
+    console.log("Ошибка", err)
+    return res.sendStatus(503);
+})
+
+
+app.listen(3000);
+console.log('http://localhost:3000');
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+let path = require('path');
+
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    entry: './index.js',
+    mode: 'development',
+    devtool: "source-map",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'script.js'
+
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.jsx?$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets:[
+                            "@babel/preset-react",
+                        ],
+                        plugins: [
+                            "@babel/plugin-proposal-class-properties",
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+};
