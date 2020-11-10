2ff1cfedab6727c5c13be7b64efad764529f5a48
SelVic <limelefthand@gmail.com>
2020-11-10 19:41:03 +0300

    mon

--- server.js
+++ server.js
@@ -39,12 +39,12 @@ const dummyData = {
 
 const newTestPost = new TestPost(dummyData)
 
-newTestPost.save((err)=>{
-    if(err)
-        console.log("Error, something happened")
-    else
-        console.log("Data has been posted")
-})
+// newTestPost.save((err)=>{
+//     if(err)
+//         console.log("Error, something happened")
+//     else
+//         console.log("Data has been posted")
+// })
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
@@ -54,26 +54,16 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/fonts', express.static(path.join(__dirname, 'fonts')));
 
-app.get("/api/data", function (req, res, next) {
-    const arr = []
-    res.json(
-        arr
-    );
+app.get("/api", function (req, res) {
+    TestPost.find({ })
+        .then((dummyData)=>{
+            res.json(data)
+        })
+        .catch((dummyData)=>{
+
+        })
 });
 
-// const getData = async () => {
-//     const res = await fetch('localhost:3000/api/get')
-//     if(res.status == 200) {
-//         const { data } = await res.json()
-//         setItems(data.items) // положить в стэйт
-//     }
-// }
-
-
-
-// app.post("/api/add", bodyParser.json(), function(req, res) {
-//
-// });
 
 app.use(
     "/",
@@ -81,18 +71,6 @@ app.use(
         res.sendFile("index.html", { root: __dirname });
     }
 );
-
-
-
-
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// app.get('/error', function (req, res, next) {
-//     next('404040')
-// })
-
 
 app.get('*', function (req, res, next) {
     console.log(`404 ${req.url}`)
@@ -107,3 +85,27 @@ app.use(function(err, req, res, next){
 
 app.listen(3000);
 console.log('http://localhost:3000');
+
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get('/error', function (req, res, next) {
+//     next('404040')
+// })
+
+
+// const getData = async () => {
+//     const res = await fetch('localhost:3000/api/get')
+//     if(res.status == 200) {
+//         const { data } = await res.json()
+//         setItems(data.items) // положить в стэйт
+//     }
+// }
+
+
+
+// app.post("/api/add", bodyParser.json(), function(req, res) {
+//
+// });
